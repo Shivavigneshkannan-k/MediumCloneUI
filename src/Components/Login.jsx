30110207cfb9e4e1032bf75327701c27872d2ce4
Shiva vignesh kannan.K <135090729+Shivavigneshkannan-k@users.noreply.github.com>
2025-05-28 22:17:05 +0530

    Login/SignUp ui completed

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -9,45 +9,49 @@ const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const user = useSelector(store => store.user);
-  
-  useEffect(()=>{
-    if(user){
-      navigate('/feed');
+  const user = useSelector((store) => store.user);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/feed");
     }
-  },[navigate,dipatch,user])
+  }, [navigate, dipatch, user]);
 
   const handleSubmit = async () => {
-    try{
+    try {
       let data = "";
-      if(isLogin){
-        data = await axios.post(API+"/login",{
-          emailId: form.emailId.toLowerCase(),
-          password: form.password
-        },{withCredentials: true})
-      }
-      else{
-       data =  await axios.post(API+"/signup",{
-          emailId: form.emailId.toLowerCase(),
-          password: form.password,
-          username: form.userName
-        },{withCredentials: true})
+      if (isLogin) {
+        data = await axios.post(
+          API + "/login",
+          {
+            emailId: form.emailId.toLowerCase(),
+            password: form.password
+          },
+          { withCredentials: true }
+        );
+      } else {
+        data = await axios.post(
+          API + "/signup",
+          {
+            emailId: form.emailId.toLowerCase(),
+            password: form.password,
+            username: form.userName
+          },
+          { withCredentials: true }
+        );
       }
       dispatch(addUser(data.data.data));
-      navigate('/feed')
-    }
-    catch(err){
+      navigate("/feed");
+    } catch (err) {
       console.log(err);
       dipatch({
-          type: "UPDATE_FIELD",
-          value:  err?.response?.data?.message || err?.message,
-          field: "error"
-        })
+        type: "UPDATE_FIELD",
+        value: err?.response?.data?.message || err?.message,
+        field: "error"
+      });
     }
-    
-    
   };
-  const [isLogin,setLogin] = useState(true);
+  const [isLogin, setLogin] = useState(true);
   const handleChange = (e) => {
     dipatch({
       type: "UPDATE_FIELD",
@@ -57,52 +61,61 @@ const Login = () => {
   };
 
   return (
-    <div className='bg-blue-300 w-full h-dvh flex justify-center items-center  '>
-      <div className='w-[50%] flex h-[60%] rounded-2xl justify-center shadow-xl'>
-        <img
-          src={SKY_IMAGE}
-          alt='sky image'
-          className='w-[45%] h-full rounded-l-2xl object-cover'
-        />
-        <div className='bg-gray-100 w-[55%] rounded-r-2xl flex flex-col justify-center'>
-          <h1 className='text-center mt-5 text-2xl'>{isLogin ? "Login into account":"Create an account"}</h1>
-          <div className='flex flex-col h-[80%] gap-3 items-center justify-center'>
+    <div className='bg-slate-200 w-full h-dvh flex justify-center items-center font-serif '>
+      <div className='lg:w-1/3 lg:h-2/3 rounded-xl p-4 bg-white shadow-xl flex flex-col '>
+        <h1 className='text-center my-5 text-3xl'>
+          {isLogin ? "Login into account" : "Create an account"}
+        </h1>
+        <div className='lg:max-h-1/2 mx-4 p-4 flex flex-col flex-grow items-center pt-[15%] space-y-5 '>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='emailId'
+            value={form.emailId}
+            onChange={(e) => handleChange(e)}
+            className='lg:w-3/4 px-4 py-2 rounded-lg focus:outline-blue-400 focus:outline-2 outline-1 outline-gray-400'></input>
+          {!isLogin && (
             <input
-              type='email'
-              placeholder='Email Address'
-              name='emailId'
-              value={form.emailId}
-              onChange={(e) => handleChange(e)}
-              className='border border-gray-400 w-[70%] rounded-xl focus:outline-2 focus:border-0 focus:outline-blue-500 px-4 py-2'></input>
-           {!isLogin &&  <input
               type='text'
               value={form.userName}
               name='userName'
               onChange={(e) => handleChange(e)}
               placeholder='Username'
-              className='border border-gray-400 w-[70%] rounded-xl ffocus:outline-2 focus:border-0 focus:outline-blue-500 px-4 py-2'></input>}
-            <input
-              type='password'
-              placeholder='Password'
-              name='password'
-              value={form.password}
-              onChange={(e) => handleChange(e)}
-              className='border border-gray-400 w-[70%] rounded-xl focus:outline-2 focus:border-0 focus:outline-blue-500 px-4 py-2'></input>
-            {form.error && <p className="text-red-600">Error: {form.error}</p>}
-            <button
-              className='py-2 px-2 bg-blue-500 w-[70%] rounded-2xl text-gray-50 mt-10 hover:bg-blue-700 '
-              onClick={() => {
-                handleSubmit();
-              }}>
-              {isLogin ?"LOGIN":"Sign Up"}
-            </button>
-          </div>
-          <div className='mx-auto mb-5'>
-            <span className='pr-2 text-center'>{isLogin?"Don't have an account?":"Have an account"}</span>
-            <a className='text-center text-blue-500 hover:underline' onClick={()=>{
-              setLogin(prev => !prev)
+              className='lg:w-3/4 px-4 py-2 rounded-lg focus:outline-blue-400 focus:outline-2 outline-1 outline-gray-400 '></input>
+          )}
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={form.password}
+            onChange={(e) => handleChange(e)}
+            className='lg:w-3/4 px-4 py-2 rounded-lg focus:outline-blue-400 focus:outline-2 outline-1 outline-gray-400'></input>
+        </div>
+        <div className="mx-6 flex flex-col justify-center flex-grow">
+          <button
+            className='w-3/4 bg-blue-400 px-4 py-2 rounded-lg
+              text-white hover:shadow-sm hover:bg-blue-500 
+              transition hover:scale-[102%] hover:ease-in hover:1s mx-auto '
+            onClick={() => {
+              handleSubmit();
             }}>
-              {isLogin ? "Register here":"Login here"}
+            {isLogin ? "LOGIN" : "SIGN UP"}
+          </button>
+          {form.error && (
+            <p className='text-red-600 opacity-80 line-clamp-1'>
+              Error: {form.error}
+            </p>
+          )}
+          <div className='m-auto text-center '>
+            <span className='pr-2 text-center'>
+              {isLogin ? "Don't have an account?" : "Have an account"}
+            </span>
+            <a
+              className='text-center text-blue-500 hover:underline'
+              onClick={() => {
+                setLogin((prev) => !prev);
+              }}>
+              {isLogin ? "Register here" : "Login here"}
             </a>
           </div>
         </div>
